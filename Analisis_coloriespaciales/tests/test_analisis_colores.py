874eac42ef6ba45224ae6197f3b99d253439c8cc
DanielZBV <danielzb@lcg.unam.mx>
2024-06-05 13:59:44 -0600

    Un nuevo test

--- Analisis_coloriespaciales/tests/test_analisis_colores.py
+++ Analisis_coloriespaciales/tests/test_analisis_colores.py
@@ -1,7 +1,7 @@
 # tests/test_analyze.py
 
 import unittest
-from Analisis_coloriespaciales.scripts.analisis_color import merge_similar_colors
+from Analisis_coloriespaciales.scripts.analisis_color import merge_similar_colors, round_color_value
 
 class TestAnalyze(unittest.TestCase):
     def test_merge_similar_colors(self):
@@ -9,6 +9,14 @@ class TestAnalyze(unittest.TestCase):
         merged = merge_similar_colors(colors, threshold=5)
         expected = {(20, 20, 20): 2, (25, 25, 25): 1, (30, 30, 30): 1}
         self.assertEqual(merged, expected)
+
+
+class TestUtils(unittest.TestCase):
+    def test_round_color_value(self):
+        rounded = round_color_value(28, round_to=10)
+        self.assertEqual(rounded, 30)
+
+
 
 if __name__ == '__main__':
     unittest.main()
